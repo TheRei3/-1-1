f5ea4dad49d529e4031122eb65b43ca12f892eae
bitcamp <bitcamp@DESKTOP-UA6S0CR>
2021-03-24 12:05:41 +0900

    1-1

--- text/src/text/text.java
+++ text/src/text/text.java
@@ -1,5 +1,28 @@
 package text;
 
 public class text {
+	
+		
+		// 13p  (page)
+		
+	    /* 
+	      Date     : 2016.1.3
+	      Source   : Hello.java
+	      Author   : 남궁석
+	      Email    : castello@naver.com
+	     */
+		
+		public static void main(String[] args) {  // 프로그램 시작
+			
+			
+			 System.out.println("Hello, world");  // Hello,word 출력
+			
+			
+		}
+		
+		
+		
+		
+		
+	}
 
-}
